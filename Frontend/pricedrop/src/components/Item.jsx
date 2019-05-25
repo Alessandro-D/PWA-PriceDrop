df15b284ba5ebee85beb610d02a76759e9680c41
xandro <a_dangeli91@hotmail.it>
2019-05-25 22:32:44 +0200

    item list

--- Frontend/pricedrop/src/components/Item.jsx
+++ Frontend/pricedrop/src/components/Item.jsx
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 // import ReactDom from 'react-dom';
 import Button from '@material-ui/core/Button';
 import { green } from '@material-ui/core/colors';
-import { Card, CardActionArea, CardMedia } from '@material-ui/core';
+import { Card, CardActionArea, CardMedia, Grid, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles({
-    button : {
-        backgroundColor : 'white',
-    },        
-    card : {
-        maxWidth: 340,
+    button: {
+        backgroundColor: 'white',
     },
-    cardMedia : {
-        height:140,
+    card: {
+        width: 340,
+    },
+    cardMedia: {
+        height: 140,
     }
 })
 
@@ -21,32 +21,39 @@ const classes = useStyles;
 
 class Item extends Component {
     state = {
-        count : 0,
-        item : this.props.item, 
+        count: 0,
+        item: this.props.item,
     }
 
     classes = useStyles;
-    
+
     handleIncrement = () => {
-        this.setState({count : this.state.count+1})
+        this.setState({ count: this.state.count + 1 })
     }
 
 
     render() {
         return (
-        <React.Fragment>
-            <Card className={classes.card} style={{width:360}}>
-                <CardActionArea>
-                    <CardMedia
-                        className={classes.cardMedia}
-                        image={this.state.item.image_url}
-                        title="Item"
-                        style={{height:140}}
-                    />
-                </CardActionArea>
-                <Button variant="contained" color="secondary" onClick={this.handleIncrement} raised >Count: {this.state.count}</Button>
-            </Card>
-        </React.Fragment>);
+            <React.Fragment>
+                <Grid item md={8}>
+                    <Card className={classes.card}>
+                        <CardActionArea>
+                            <CardMedia
+                                className={classes.cardMedia}
+                                image={this.state.item.image_url}
+                                title="Item"
+                                style={{ height: 140 }}
+                                md={12}
+                            />
+                        </CardActionArea>
+                        <Button variant="contained" color="secondary" onClick={this.handleIncrement} raised>Count: {this.state.count}</Button>
+                    </Card>
+                </Grid>
+                <Grid item md={4}>
+                    <Box md={4}><a>side link</a></Box>
+                </Grid>
+            </React.Fragment>
+        );
     }
 }
 
--- Frontend/pricedrop/src/components/Itemlist.jsx
+++ Frontend/pricedrop/src/components/Itemlist.jsx
@@ -1,23 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Item from './Item';
+import { Grid } from '@material-ui/core';
 
 class ItemList extends Component {
-    state = {  }
+    state = {}
 
-    render() { 
-        return ( 
+    render() {
+        return (
             <React.Fragment>
-                {items.map(item =>(<Item key={item.id} item={item}></Item>))}
+                <Grid container spacing={2}>
+                    {items.map(item => (<Item key={item.id} item={item}></Item>))}
+                </Grid>
             </React.Fragment>
-         );
+        );
     }
 }
- 
+
 export default ItemList;
 
 const items = [
     {
-        id:1,
+        id: 1,
         name: 'Item name',
         url: 'url',
         image_url: 'https://material-ui.com/static/images/cards/contemplative-reptile.jpg',
@@ -27,7 +30,7 @@ const items = [
         date_added: 'date'
     },
     {
-        id:2,
+        id: 2,
         name: 'Item name',
         url: 'url',
         image_url: 'https://material-ui.com/static/images/cards/contemplative-reptile.jpg',
@@ -37,7 +40,7 @@ const items = [
         date_added: 'date'
     },
     {
-        id:3,
+        id: 3,
         name: 'Item name',
         url: 'url',
         image_url: 'https://material-ui.com/static/images/cards/contemplative-reptile.jpg',
@@ -45,8 +48,8 @@ const items = [
         current_price: 110,
         price_query: 'default',
         date_added: 'date'
-    },{
-        id:4,
+    }, {
+        id: 4,
         name: 'Item name',
         url: 'url',
         image_url: 'https://material-ui.com/static/images/cards/contemplative-reptile.jpg',

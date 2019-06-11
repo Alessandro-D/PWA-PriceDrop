cb51faf031ebf9c5221be6fc88208ec6798699c0
D'Angeli <alessandro.dangeli@capgemini.com>
2019-06-11 22:10:51 +0200

    search.css

--- Frontend/pricedrop/src/components/search/ReviewForm.jsx
+++ Frontend/pricedrop/src/components/search/ReviewForm.jsx
@@ -3,65 +3,31 @@ import TextField from "@material-ui/core/TextField";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 
+import "./_search.css";
+
 class ReviewForm extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <React.Fragment>
-                <Paper style={containerStyle}>
-            <Paper style={boxStyle}>
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <Paper className="containerStyle">
+          <Paper className="boxStyle">
             <Grid container>
               {/* justify="space-around" */}
               <TextField
                 id="standard-with-placeholder"
                 label="Insert item URL here"
                 placeholder="URL"
-                style={searchStyle}
+                className="searchStyle"
                 margin="normal"
                 // variant="filled"
               />
             </Grid>
-            </Paper>
+          </Paper>
         </Paper>
-            </React.Fragment>
-         );
-    }
+      </React.Fragment>
+    );
+  }
 }
- 
-export default ReviewForm;
 
-const containerStyle = {
-    marginBottom: "20px",
-    // padding: "5px",
-    boxShadow: "0px 1px 3px 0px",
-    backgroundColor: "#e5e5e5",
-    width: "80%"
-  };
-  
-  const searchStyle = {
-    margin: 5,
-    // width: "100%",
-    backgroundColor: "#FFFFFF",
-    width: "100%",
-    margin: "10px"
-  };
-  
-  const boxStyle = {
-    backgroundColor: "#FFFFFF",
-    padding: 0,
-    width: "60%",
-    margin: "10px",
-    innerHeight: 50
-    // boxShadow: "inset 1px 1px 3px 1px"
-  };
-  
-  const buttonStyle = {
-    // width: 10,
-    display: "inline",
-    marginTop: 10,
-    marginBottom: 20,
-    backgroundColor: "#227231",
-    color: "#FFFFFF",
-    height:50,
-  };
-  
+export default ReviewForm;
